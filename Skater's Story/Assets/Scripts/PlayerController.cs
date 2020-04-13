8058bf9e6f5cdb5908f6d9e6aa873ef576115978
Thomas Theux <t.grikschas@gmail.com>
2020-04-13 23:22:58 +0200

    Started implementing player controller

---
+++ Skater's Story/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private Player player;
+
+    public int playerID = 0;
+
+    public float push = 1.0f;
+
+    // REWIRED
+    private bool xBTN = false;
+
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        player = ReInput.players.GetPlayer(playerID);
+    }
+
+
+    private void Update() {
+        GetInput();
+
+        if (xBTN) {
+            MoveSkater();
+        }
+
+        print(rb.velocity.magnitude);
+    }
+
+
+    private void GetInput() {
+        xBTN = player.GetButton("X");
+    }
+
+
+    private void MoveSkater() {
+        rb.AddForce(transform.right * push);
+    }
+
+}
