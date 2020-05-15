fdb241d9879faff33ef6c64e6a70727b3fc39f23
Thomas Theux <t.grikschas@gmail.com>
2020-05-15 18:32:53 +0200

    Started implementing balancing

---
+++ Skater's Story/Assets/Scripts/BalanceController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BalanceController : MonoBehaviour {
+
+    public TricksHandler TricksHandlerScript;
+
+    public GameObject GrindingGO;
+    private Slider GrindSlider;
+
+
+    private void Awake() {
+        GrindSlider = GrindingGO.GetComponent<Slider>();
+    }
+
+
+    private void Update() {
+    }
+
+}
